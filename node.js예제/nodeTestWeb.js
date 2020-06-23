b53341a895371befa77b8b599afc6a646a77b579
DESKTOP-ARTH003\LIMEFRIENDS <sangkomi@limefriends.com>
2020-06-23 18:54:56 +0900

    기능 분리

--- node.js예제/nodeTestWeb.js
+++ node.js예제/nodeTestWeb.js
@@ -14,16 +14,23 @@ var webStop = function(){ // 웹서버 종료
 	return console.log('웹서버 중지'); 
 };
 
-var createWeb = function(req,res){ // 웹서버 생성
-    var requestURL = url.parse(req.url).query;
-    var param = queryString.parse(requestURL , '&'); //&로 구분하여 객체로 만듬
+// 웹서버 생성 //
+var createWeb = function(req,res){
+	requestURL = url.parse(req.url).query; // var 제거 전역 변수로 선언 > 아래 HTML 분리 부분에서 requestURL사용
     res.setHeader('Content-Type','text/html;charset=utf-8');
-    var html = ``;
-    if(requestURL != null){
+    res.write(webHtml() );
+}
+
+// HTML 분리 //
+var webHtml  = function(){	
+	var param = queryString.parse(requestURL , '&'); //&로 구분하여 객체로 만듬
+	var html = ``;
+	 if(requestURL != null){
         html = `<div>UserName :  ${param.username}~</div>
                 <br>
                 <div>PhoneNumber : ${param.phonenumber}~</div>
-                <script>console.log('메롱');</script>`;        
+                <script>console.log('콘솔 출력');</script>`;    
+		 return html;
     }else{        
         html = `<form action="/input">
                     <label for="username">회원이름</label>
@@ -31,11 +38,10 @@ var createWeb = function(req,res){ // 웹서버 생성
                     <lable for="phonenumber">핸드폰번호</label>
                     <input type="text" name="phonenumber" id="phonenumber"></input>
                     <input type="submit" value="요청보내기">
-                    <script>console.log('메롱');</script>
-                </form>`;
-               
+                    <script>console.log('콘솔 출력');</script>
+                </form>`;    
+		return html;
     }
-    res.write(html);
 }
 
 // 실행문 //
