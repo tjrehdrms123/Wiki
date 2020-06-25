a11dcb0fbb4d0080e5b1fd9c980da57e4cafbf7a
DESKTOP-ARTH003\LIMEFRIENDS <sangkomi@limefriends.com>
2020-06-25 18:27:49 +0900

    익스프레스 서버 생성 연습

---
+++ node.js예제/익스프레스 서버 생성 연습/expressWeb.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var http = require('http');
+
+// 익스프레스 객체 생성
+var app = express();
+
+// 기본포트 설정
+app.set('port',process.evt.PORT || 3000);
+
+// 익스프레스 서버 시작
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('익스프레스 웹 서버 시작')
+});
